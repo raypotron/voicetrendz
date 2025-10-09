f96c9c2d5b952c25ee74983de838f1ac3925d789
Usenmfon <usenmfonuko@gmail.com>
2025-10-09 12:10:32 +0100

    update: deploy scripts

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,6 +1,7 @@
-import { dashboard, login, register } from '@/routes';
+// import { dashboard, login, register } from '@/routes';
 import { type SharedData } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/react';
+// import { route } from 'ziggy-js';
 
 export default function Welcome() {
     const { auth } = usePage<SharedData>().props;
@@ -19,7 +20,7 @@ export default function Welcome() {
                     <nav className="flex items-center justify-end gap-4">
                         {auth.user ? (
                             <Link
-                                href={dashboard()}
+                                href=''
                                 className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                             >
                                 Dashboard
@@ -27,13 +28,13 @@ export default function Welcome() {
                         ) : (
                             <>
                                 <Link
-                                    href={login()}
+                                    href=''
                                     className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
                                 >
                                     Log in
                                 </Link>
                                 <Link
-                                    href={register()}
+                                    href=''
                                     className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                                 >
                                     Register
