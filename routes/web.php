184d1180d0eb91d4c199e7a33ff68f23e962c0f4
Usenmfon <usenmfonuko@gmail.com>
2025-10-09 14:32:40 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,11 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('dashboard', function () {
+//         return Inertia::render('dashboard');
+//     })->name('dashboard');
+// });
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
