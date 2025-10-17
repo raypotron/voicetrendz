cdcd7da5141ecb7a902b81d4419630b9881ab3a2
Usenmfon <usenmfonuko@gmail.com>
2025-10-17 15:07:45 +0100

    update: route

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ Route::post('editor/upload', UploadController::class)->name('editor.upload');
 //     })->name('dashboard');
 // });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+// require __DIR__.'/settings.php';
+// require __DIR__.'/auth.php';
