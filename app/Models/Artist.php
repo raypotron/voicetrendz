da8ec5c1a3b0a1b19f470fc3dac7d6b2a86b46f4
Usenmfon <usenmfonuko@gmail.com>
2025-10-19 00:48:47 +0100

    update: create relationship

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -13,4 +13,9 @@ class Artist extends Model
         'bio',
         'image_url'
     ];
+
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class, 'artist_genre');
+    }
 }
