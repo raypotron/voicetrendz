6478870ce9258984566a1ca8e6746e04d1ad621f
Usenmfon <usenmfonuko@gmail.com>
2025-10-19 00:52:09 +0100

    update: create relationship

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Genre extends Model
 {
     protected $fillable = ['name'];
+
+    public function artists()
+    {
+        return $this->belongsToMany(Artist::class, 'artist_genre');
+    }
 }
