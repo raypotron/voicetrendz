88cce6080e3714251f38f3aeb81885001d8f2296
Usenmfon <usenmfonuko@gmail.com>
2025-10-23 08:23:55 +0100

    update

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -96,7 +96,7 @@ export default function Home() {
             </p>
             <Link
               href=""
-              className="px-6 py-3 bg-purple-600 hover:bg-purple-700 text-white rounded-lg font-semibold flex items-center gap-2 transition"
+              className="px-6 py-3 bg-purple-600 hover:bg-purple-700 w-fit text-white rounded-lg font-semibold flex items-center gap-2 transition"
             >
               Read Full Story <ChevronRight className="w-4 h-4" />
             </Link>
