8d1d8a369355248bf0be29910ac65396edfc9747
Usenmfon <usenmfonuko@gmail.com>
2025-10-26 01:11:03 +0100

    update: ui

--- app/Http/Controllers/Blog/WelcomeController.php
+++ app/Http/Controllers/Blog/WelcomeController.php
@@ -4,23 +4,26 @@ namespace App\Http\Controllers\Blog;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Services\PostService;
 use Inertia\Inertia;
 
 class WelcomeController extends Controller
 {
-    public function __construct(private Post $post) {}
+    public function __construct(private Post $post, private PostService $postService) {}
 
     public function __invoke()
     {
-        $heroPost = $this->post->where('status', 'published')
-            ->whereHas('tags', function ($query) {
-                $query->whereIn('name', ['breaking news']);
-            })->with('tags:id,name')->latest()->first();
+        $heroPost = $this->postService->getPostsByTag('breaking news')->first();
+        $hotStories =$this->postService->getPostsByTag('hottest', 4);
+        // $heroPost = $this->post->where('status', 'published')
+        //     ->whereHas('tags', function ($query) {
+        //         $query->whereIn('name', ['breaking news']);
+        //     })->with('tags:id,name')->latest()->first();
 
-        $hotStories = $this->post->where('status', 'published')
-            ->whereHas('tags', function ($query) {
-                $query->whereIn('name', ['hottest']);
-            })->with('tags:id,name')->latest()->limit(4)->get();
+        // $hotStories = $this->post->where('status', 'published')
+        //     ->whereHas('tags', function ($query) {
+        //         $query->whereIn('name', ['hottest']);
+        //     })->with('tags:id,name')->latest()->limit(4)->get();
 
         // dd($hotStories);
 
---
+++ app/Services/PostService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
+
+class PostService
+{
+    public function getPostsByTag(string $tag, ?int $limit = null)
+    {
+        // $key = "posts_{$tag}_".($limit ?? 'all');
+
+        // return Cache::remember($key, now()->addMinutes(10), function () use ($tag, $limit) {
+            $query = Post::where('status', 'published')
+                ->whereHas('tags', fn ($q) => $q->where('name', $tag))
+                ->with(['tags:id,name', 'category:id,name'])
+                ->latest();
+
+            if ($limit) {
+                $query->limit($limit);
+            }
+
+            return $query->get();
+        // });
+
+    }
+}
