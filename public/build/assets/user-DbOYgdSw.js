3ee87b1adcbe5865ef6047fa90b98eb8b8631516
Usenmfon <usenmfonuko@gmail.com>
2025-11-10 08:23:05 +0100

    update: build files

--- public/build/assets/user-DbOYgdSw.js
+++ public/build/assets/user-DbOYgdSw.js
@@ -1,4 +1,4 @@
-import{c as e}from"./app-CR9zQNVN.js";/**
+import{c as e}from"./app-qBm6elX8.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
