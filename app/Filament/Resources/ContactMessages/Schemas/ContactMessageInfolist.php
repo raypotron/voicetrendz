7628261144816613c6f288d63ec8fc4fb47faf05
Usenmfon <usenmfonuko@gmail.com>
2025-11-28 16:17:02 +0100

    feat(resources): create contact message resources

---
+++ app/Filament/Resources/ContactMessages/Schemas/ContactMessageInfolist.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\ContactMessages\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class ContactMessageInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('name'),
+                TextEntry::make('email')
+                    ->label('Email address'),
+                TextEntry::make('subject'),
+                TextEntry::make('to'),
+                TextEntry::make('created_at')
+                    ->dateTime(),
+                TextEntry::make('updated_at')
+                    ->dateTime(),
+            ]);
+    }
+}
