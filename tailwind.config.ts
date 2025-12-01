bf264a9f4ff7ad665dd302c76284bf40ef5da3b1
Usenmfon <usenmfonuko@gmail.com>
2025-12-01 15:08:10 +0100

    changed tailwin config setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,24 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.jsx',
-    './resources/**/*.ts',
-    './resources/**/*.tsx',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [typography],
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.jsx',
+        './resources/**/*.ts',
+        './resources/**/*.tsx',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                card: 'var(--card-body)',
+                brand: {
+                    1: '#060A1A',
+                    2: '#0A102A',
+                    3: '#050515',
+                },
+            },
+        },
+    },
+    plugins: [typography],
 };
