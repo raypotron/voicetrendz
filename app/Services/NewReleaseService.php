d58fc33b3cb0c584ce81731abb022546dbc7f4d5
Usenmfon <usenmfonuko@gmail.com>
2025-12-11 04:35:21 +0100

    update: use paginate method to retrieve paginated result

--- app/Services/NewReleaseService.php
+++ app/Services/NewReleaseService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\MusicVideo;
 use App\Models\Song;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class NewReleaseService
 {
@@ -17,9 +18,41 @@ class NewReleaseService
 
         $results = $songs->merge($videos)->sortByDesc('created_at')->values();
 
-        if($limit) $results = $results->take($limit);
+        if ($limit) {
+            $results = $results->take($limit);
+        }
 
         return $results;
 
+    }
+
+    public function index(?int $limit = 15)
+    {
+        $songs = $this->song->where('status', 'published')
+            ->latest()
+            ->get();
+
+        $videos = $this->musicVideo
+            ->with('artist')
+            ->latest()
+            ->get();
+
+        $results = $songs->merge($videos)
+            ->sortByDesc('created_at')
+            ->values();
+
+        $page = request()->get('page', 1); // current page
+        $perPage = $limit ?? 15;
+        $total = $results->count();
+
+        $paginated = new LengthAwarePaginator(
+            $results->forPage($page, $perPage),
+            $total,
+            $perPage,
+            $page,
+            ['path' => request()->url(), 'query' => request()->query()]
+        );
+
+        return $paginated;
     }
 }
