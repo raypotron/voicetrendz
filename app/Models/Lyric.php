223794e6a4f2caa588ac1355f9604271c17e5338
Usenmfon <usenmfonuko@gmail.com>
2025-12-12 22:37:00 +0100

    update: use factory

--- app/Models/Lyric.php
+++ app/Models/Lyric.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Lyric extends Model
 {
+    use HasFactory;
+    
     protected $fillable = [
         'title',
         'slug',
