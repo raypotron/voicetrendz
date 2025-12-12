4ade6ec967935125b07f9883cd210d89dfa48947
Usenmfon <usenmfonuko@gmail.com>
2025-12-12 20:42:11 +0100

    detect uncaught lazy loaders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
@@ -28,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
+
+        Model::preventLazyLoading(! app()->isProduction());
 
         Inertia::share([
             // 'user' => fn () => Auth::user()?->only(['id', 'name']),
