8eca3f55200e26e8db582e8c87038c2f912f3a40
Usenmfon <usenmfonuko@gmail.com>
2025-12-13 22:36:32 +0100

    implementation logic for socialite

---
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialiteController extends Controller
+{
+    public function redirect(string $provider)
+    {
+        $this->validateProvider($provider);
+
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function callback(string $provider)
+    {
+        $this->validateProvider($provider);
+        $response = Socialite::driver($provider)->user();
+
+        $user = User::firstWhere(['email' => $response->getEmail()]);
+
+        if ($user) {
+            if (! $user->{$provider.'_id'}) {
+                $user->update([
+                    $provider.'_id' => $response->getId(),
+                ]);
+            }
+        } else {
+            $user = User::create([
+                $provider.'_id' => $response->getId(),
+                'name' => $response->getName() ?? $response->getNickname(),
+                'email' => $response->getEmail(),
+                'password' => bcrypt(str()->random(32)),
+            ]);
+        }
+
+        Auth::login($user);
+
+        return redirect()->intended('user');
+
+    }
+
+    protected function validateProvider(string $provider): void
+    {
+        if (! in_array($provider, ['google'])) {
+            throw ValidationException::withMessages([
+                'provider' => 'Unsupported authentication provider.',
+            ]);
+        }
+    }
+}
