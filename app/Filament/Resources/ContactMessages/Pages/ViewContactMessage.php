b682a245c2622c1068c059e72b5e9005f905f1e0
Usenmfon <usenmfonuko@gmail.com>
2025-12-13 13:14:05 +0100

    feat(notification): integrate db and mail notification

--- app/Filament/Resources/ContactMessages/Pages/ViewContactMessage.php
+++ app/Filament/Resources/ContactMessages/Pages/ViewContactMessage.php
@@ -10,10 +10,19 @@ class ViewContactMessage extends ViewRecord
 {
     protected static string $resource = ContactMessageResource::class;
 
+    public function mount($record): void
+    {
+        parent::mount($record);
+
+        if (is_null($this->record->read_at)) {
+            $this->record->markAsRead();
+        }
+    }
+
     protected function getHeaderActions(): array
     {
         return [
-            EditAction::make(),
+            // EditAction::make(),
         ];
     }
 }
